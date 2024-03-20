6f2cc00eb185e24b6a3b6237c2f95d619c228ad0
Daniel Schmelz <daniel@schmelz.org>
2024-03-20 08:49:59 +0100

    FIX: escape html

--- views/licence-page.php
+++ views/licence-page.php
@@ -1,15 +1,15 @@
 <div class="wrap">
-    <h1><?= esc_html(get_admin_page_title()); ?></h1>
+    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
     <form method="post"
-          action="<?= esc_attr(wp_nonce_url(admin_url("admin-post.php"), "aisidekick_update_licence_key")); ?>">
+          action="<?php echo esc_attr(wp_nonce_url(admin_url("admin-post.php"), "aisidekick_update_licence_key")); ?>">
         <div id="universal-message-container">
             <h2>AI Sidekick</h2>
             <div class="options">
                 <p>
                     <label for="license_key">Licence key:</label>
                     <input id="license_key" type="password" name="aisidekick-licence-key"
-                           value="<?= esc_attr(get_option("aisidekick_licence_key", "")); ?>" /><br />
-                    <span style="color:<?= esc_attr($messageColor); ?>"><?= $messageToShow; ?></span>
+                           value="<?php echo esc_attr(get_option("aisidekick_licence_key", "")); ?>" /><br />
+                    <span style="color:<?php echo esc_attr($messageColor); ?>"><?php echo wp_kses_post($messageToShow); ?></span>
                 </p>
             </div><!-- #universal-message-container -->
             <?php
