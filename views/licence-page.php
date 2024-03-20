2d34c11bb41173abcad8894f84a5bca2101769d6
Daniel Schmelz <daniel@schmelz.org>
2024-03-20 08:56:56 +0100

    FIX: prevent direct file access

--- views/licence-page.php
+++ views/licence-page.php
@@ -1,3 +1,10 @@
+<?php
+
+if (!defined("ABSPATH")) {
+    exit();
+} // Exit if accessed directly
+?>
+
 <div class="wrap">
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
     <form method="post"
